ce30d2becc5bed1a2eb253239764154d55583260
Rickard Andersson <dunklas@gmail.com>
2023-12-17 23:13:17 +0100

    wip

--- src/y2023/day12.rs
+++ src/y2023/day12.rs
@@ -5,12 +5,55 @@ pub fn solve(input: &str) {
 
 fn part1(input: &str) -> usize {
     let x = parse(input);
-    println!("{:?}", x);
+    let (row, conditions) = x.first().unwrap();
+    is_valid(row, conditions);
+    solve_row(row.clone(), conditions);
     0
 }
 
 fn part2(input: &str) -> usize {
     0
+}
+
+fn solve_row(row: String, conditions: &Vec<u8>) -> usize {
+    if !row.contains('?') {
+        return 1;
+    }
+    solve_row(row.replacen('?', ".", 1), conditions)
+        + solve_row(row.replacen('?', "#", 1), conditions)
+}
+
+fn is_valid(row: &str, conditions: &Vec<u8>) -> bool {
+    let mut iter = row.chars();
+    let mut states: Vec<(State, u8)> = Vec::new();
+    let mut prev_state = None;
+    let mut state_len = 0;
+    while let Some(next) = iter.next() {
+        let current_state = match next {
+            '#' => State::Damaged,
+            '.' => State::Operational,
+            '?' => State::Unknown,
+            _ => panic!("Unexpected char")
+        };
+        if let Some(prev) = prev_state {
+            if prev != current_state {
+                states.push((prev, state_len));
+                state_len = 0;
+            }
+        }
+        prev_state = Some(current_state);
+        state_len += 1;
+    }
+    if let Some(prev) = prev_state {
+        states.push((prev, state_len));
+    }
+
+    for condition in conditions {
+
+    }
+
+    println!("{:?}", states);
+    true
 }
 
 fn parse(input: &str) -> Vec<(String, Vec<u8>)> {
@@ -28,6 +71,13 @@ fn parse(input: &str) -> Vec<(String, Vec<u8>)> {
             )
         })
         .collect()
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+enum State {
+    Unknown,
+    Damaged,
+    Operational
 }
 
 #[cfg(test)]
