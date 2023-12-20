8d24d636709cc4d9ec2d8ed8fd4c6c96dcf1ee1e
Rickard Andersson <dunklas@gmail.com>
2023-12-20 06:53:31 +0100

    skeleton

---
+++ src/y2023/day16.rs
@@ -0,0 +1,32 @@
+pub fn solve(input: &str) {
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> u32 {
+    0
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = ".|...\\....
+|.-.\\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\\\..
+.-.-/..|..
+.|....-|.\\
+..//.|....";
+        assert_eq!(part1(input), 46);
+    }
+}
--- src/y2023/mod.rs
+++ src/y2023/mod.rs
@@ -3,6 +3,7 @@ pub mod day10;
 pub mod day11;
 pub mod day14;
 pub mod day15;
+pub mod day16;
 pub mod day2;
 pub mod day3;
 pub mod day4;
