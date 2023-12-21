26af07001e0f252923c3fc107494e0c7a3431649
Rickard Andersson <dunklas@gmail.com>
2023-12-21 08:33:29 +0100

    part 1 works for test

--- src/y2023/day21.rs
+++ src/y2023/day21.rs
@@ -1,19 +1,49 @@
+use std::collections::{HashSet, VecDeque};
+
 pub fn solve(input: &str) {
     println!("Part 1: {}", part1(input, 64));
     println!("Part 2: {}", part2(input));
 }
 
-fn part1(input: &str, steps: u32) -> u32 {
+fn part1(input: &str, steps: u32) -> usize {
     let garden = parse(input);
     let start = start_pos(&garden).unwrap();
-    0
+    num_plots(start, &garden, steps)
 }
 
 fn part2(input: &str) -> u32 {
     0
 }
 
-fn num_plots(steps: u32, pos: (usize, usize), garden: &[Vec<char>]) {}
+fn num_plots(start: (usize, usize), garden: &[Vec<char>], desired_steps: u32) -> usize {
+    let mut to_visit = vec![(start, 0)].into_iter().collect::<VecDeque<_>>();
+    let mut targets = HashSet::<(usize, usize)>::new();
+    while let Some(((y, x), distance)) = to_visit.pop_front() {
+        if distance == desired_steps {
+            targets.insert((y, x));
+            continue;
+        }
+        let mut neighbours = vec![];
+        if let Some(left) = x.checked_sub(1) {
+            neighbours.push((y, left));
+        }
+        if let Some(up) = y.checked_sub(1) {
+            neighbours.push((up, x));
+        }
+        if x + 1 < garden[0].len() {
+            neighbours.push((y, x + 1));
+        }
+        if y + 1 < garden.len() {
+            neighbours.push((y + 1, x));
+        }
+        for (ny, nx) in neighbours {
+            if garden[ny][nx] != '#' {
+                to_visit.push_back(((ny, nx), distance + 1));
+            }
+        }
+    }
+    targets.len()
+}
 
 fn start_pos(garden: &[Vec<char>]) -> Option<(usize, usize)> {
     garden
