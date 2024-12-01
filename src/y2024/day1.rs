2edbc2ceeee13ee9c34117033c91c878ebedc073
Rickard Andersson <dunklas@gmail.com>
2024-12-01 08:22:53 +0100

    improve parsing

--- src/y2024/day1.rs
+++ src/y2024/day1.rs
@@ -1,39 +1,47 @@
+use std::error::Error;
+
 pub fn solve(input: &str) {
     println!("Part 1: {}", part1(input));
     println!("Part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
-    let mut first = Vec::new();
-    let mut second = Vec::new();
-    input.lines().map(|line| line.split_whitespace().collect::<Vec<_>>())
-        .for_each(|row| {
-            first.push(row[0].parse::<usize>().unwrap());
-            second.push(row[1].parse::<usize>().unwrap());
-        });
-    first.sort();
-    second.sort();
+    let (first, second) = parse(input).unwrap();
     first.iter().zip(second.iter())
         .map(|(a, b)| a.abs_diff(*b))
         .sum()
 }
 
 fn part2(input: &str) -> usize {
-    let mut first = Vec::new();
-    let mut second = Vec::new();
-    input.lines().map(|line| line.split_whitespace().collect::<Vec<_>>())
-        .for_each(|row| {
-            first.push(row[0].parse::<usize>().unwrap());
-            second.push(row[1].parse::<usize>().unwrap());
-        });
-    first.sort();
-    second.sort();
-
+    let (first, second) = parse(input).unwrap();
     first.iter()
         .map(|n| {
             second.iter().filter(|n2| n == *n2).count() * n
         })
         .sum()
+}
+
+fn parse(input: &str) -> Result<(Vec<usize>, Vec<usize>), Box<dyn Error>> {
+    let mut first = Vec::new();
+    let mut second = Vec::new();
+
+    input.lines()
+        .filter_map(|line| {
+            let mut parts = line.split_whitespace();
+            Some((
+                parts.next()?.parse::<usize>().ok()?,
+                parts.next()?.parse::<usize>().ok()?,
+            ))
+        })
+        .for_each(|(f, s)| {
+            first.push(f);
+            second.push(s);
+        });
+
+    first.sort_unstable();
+    second.sort_unstable();
+
+    Ok((first, second))
 }
 
 #[cfg(test)]
