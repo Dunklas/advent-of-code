aec8162580457204c2437f7bd4fad212fb112930
Rickard Andersson <dunklas@gmail.com>
2024-12-03 06:21:24 +0100

    part 1

--- src/y2024/day3.rs
+++ src/y2024/day3.rs
@@ -1,10 +1,19 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref CARD: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+}
+
 pub fn solve(input: &str) {
     println!("Part 1: {}", part1(input));
     println!("Part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> usize {
-    0
+    CARD.captures_iter(input).into_iter()
+        .map(|m| m.get(1).unwrap().as_str().parse::<usize>().unwrap() * m.get(2).unwrap().as_str().parse::<usize>().unwrap())
+        .sum()
 }
 
 fn part2(input: &str) -> usize {
@@ -17,8 +26,8 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = "";
-        assert_eq!(part1(input), 0);
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(part1(input), 161);
     }
 
     #[test]
