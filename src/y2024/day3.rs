1abb056c1d8d55e75e4ba5c7bedddafed446ed6b
Rickard Andersson <dunklas@gmail.com>
2024-12-03 06:34:23 +0100

    solve part 2

--- src/y2024/day3.rs
+++ src/y2024/day3.rs
@@ -2,7 +2,8 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
-    static ref CARD: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    static ref MUL: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    static ref ALL: Regex = Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))").unwrap();
 }
 
 pub fn solve(input: &str) {
@@ -11,13 +12,34 @@ pub fn solve(input: &str) {
 }
 
 fn part1(input: &str) -> usize {
-    CARD.captures_iter(input).into_iter()
+    MUL.captures_iter(input).into_iter()
         .map(|m| m.get(1).unwrap().as_str().parse::<usize>().unwrap() * m.get(2).unwrap().as_str().parse::<usize>().unwrap())
         .sum()
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let mut enabled = true;
+    let mut result = 0;
+    let c = ALL.captures_iter(input).into_iter().for_each(|m| {
+        match m.get(0) {
+            Some(m) => {
+                let ins = m.as_str();
+                if let(Some(x)) = MUL.captures(ins) {
+                    if enabled {
+                        result += (x.get(1).unwrap().as_str().parse::<usize>().unwrap() * x.get(2).unwrap().as_str().parse::<usize>().unwrap());
+                    }
+                }
+                if ins == "do()" {
+                    enabled = true;
+                }
+                if ins == "don't()" {
+                   enabled = false;
+                }
+            }
+            None => {}
+        }
+    });
+    result
 }
 
 #[cfg(test)]
@@ -32,7 +54,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let input = "";
-        assert_eq!(part2(input), 0);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(part2(input), 48);
     }
 }
