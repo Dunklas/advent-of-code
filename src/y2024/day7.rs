c51fae05b23882d54b18d4f05e359272a860f2e4
Rickard Andersson <dunklas@gmail.com>
2024-12-07 07:57:33 +0100

    solve part 2

--- src/y2024/day7.rs
+++ src/y2024/day7.rs
@@ -1,7 +1,6 @@
-use std::panic::resume_unwind;
-use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::error::Error;
 
 lazy_static! {
     static ref EQUATION: Regex = Regex::new(r"^(\d+): (\d+) (\d+)$").unwrap();
@@ -14,56 +13,62 @@ pub fn solve(input: &str) {
 
 fn part1(input: &str) -> usize {
     parse(input)
+        .unwrap()
         .into_iter()
-        .filter(|(result, operands)| calc(result + 0, &operands, 0, 0) >= 1)
+        .filter(|(result, operands)| evaluate(*result, operands, 0, false) >= 1)
         .map(|(result, _)| result)
-        .map(|result| {
-            println!("RESULT: {}", result);
-            result
-        })
         .sum()
 }
 
 fn part2(input: &str) -> usize {
-    0
+    parse(input)
+        .unwrap()
+        .into_iter()
+        .filter(|(result, operands)| evaluate(*result, operands, 0, true) >= 1)
+        .map(|(result, _)| result)
+        .sum()
 }
 
-fn calc(result: usize, operands: &Vec<usize>, index: usize, sum: usize) -> usize {
-    if let Some(a) = operands.get(index) {
-        if a + sum == result {
-            println!("Found result: {}+{}={}", a, sum, result);
-            return 1;
-        }
-        if a * sum == result {
-            println!("Found result: {}*{}={}", a, sum, result);
-            return 1;
-        }
-        return calc(result, operands, index + 1, a + sum) + calc(result, operands, index + 1, a * sum);
+fn evaluate(value: usize, operands: &[usize], current: usize, concat_enabled: bool) -> usize {
+    let next = operands[0];
+    let sum = current + next;
+    let product = current * next;
+    let concat = format!("{}{}", current, next).parse::<usize>().unwrap();
+    if sum == value || product == value || (concat_enabled && concat == value) {
+        return 1;
     }
-    0
+    let next_operands = &operands[1..];
+    if next_operands.is_empty() {
+        return 0;
+    }
+    evaluate(value, next_operands, sum, concat_enabled)
+        + evaluate(value, next_operands, product, concat_enabled)
+        + match concat_enabled {
+            true => evaluate(value, next_operands, concat, concat_enabled),
+            false => 0,
+        }
 }
 
-fn parse(input: &str) -> Vec<(usize, Vec<usize>)> {
-    input
+fn parse(input: &str) -> Result<Vec<(usize, Vec<usize>)>, Box<dyn Error>> {
+    Ok(input
         .lines()
-        .map(|line| {
-            let parts = line.split(":").collect::<Vec<_>>();
-            let operands = parts[1]
-                .split_whitespace()
-                .filter_map(|n| n.parse::<usize>().ok())
-                .collect::<Vec<_>>();
-            (parts[0].parse().unwrap(), operands)
+        .filter_map(|line| {
+            let mut parts = line.split(":");
+            let value = parts.next().and_then(|n| n.parse::<usize>().ok());
+            let operands = parts.next().map(|raw| {
+                raw.split_whitespace()
+                    .filter_map(|w| w.parse::<usize>().ok())
+                    .collect::<Vec<_>>()
+            });
+            value.zip(operands)
         })
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::y2024::day7::part1;
-
-    #[test]
-    fn test_part1() {
-        let input = "190: 10 19
+    use crate::y2024::day7::{part1, part2};
+    const INPUT: &str = "190: 10 19
 3267: 81 40 27
 83: 17 5
 156: 15 6
@@ -72,12 +77,14 @@ mod tests {
 192: 17 8 14
 21037: 9 7 18 13
 292: 11 6 16 20";
-        assert_eq!(part1(input), 3749);
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 3749);
     }
 
     #[test]
     fn test_part2() {
-        let input = "";
-        assert_eq!(part1(input), 0);
+        assert_eq!(part2(INPUT), 11387);
     }
 }
