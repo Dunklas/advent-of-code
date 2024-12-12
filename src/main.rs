7c281b817f868326b40664f625e6ec90c4e04ae0
Rickard Andersson <dunklas@gmail.com>
2024-12-12 06:30:38 +0100

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -132,6 +132,9 @@ fn main() {
             "11" => {
                 y2024::day11::solve(&input);
             }
+            "12" => {
+                y2024::day12::solve(&input);
+            }
             _ => {
                 panic!("Solution does not exist for {} {}", year, day);
             }
