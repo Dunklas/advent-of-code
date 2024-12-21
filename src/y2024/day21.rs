f094486282e121791057ec93fdd390d9eaa2f266
Rickard Andersson <dunklas@gmail.com>
2024-12-21 06:13:42 +0100

    prep

---
+++ src/y2024/day21.rs
@@ -0,0 +1,28 @@
+pub fn solve(input: &str) {
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    0
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "029A
+980A
+179A
+456A
+379A";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 126384);
+    }
+}
