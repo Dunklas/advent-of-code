6ba7f27f7336a910798e313e59fd861f5a5f3f5b
Rickard Andersson <dunklas@gmail.com>
2024-12-24 08:11:58 +0100

    part 2

--- src/y2024/day24.rs
+++ src/y2024/day24.rs
@@ -1,10 +1,9 @@
-use std::collections::{HashMap, VecDeque};
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::{HashMap, VecDeque};
 
 lazy_static! {
-    static ref WIRE: Regex =
-        Regex::new(r"^([0-9a-z]{3}): (1|0)$").unwrap();
+    static ref WIRE: Regex = Regex::new(r"^([0-9a-z]{3}): (1|0)$").unwrap();
     static ref GATE: Regex =
         Regex::new(r"^([0-9a-z]{3}) ([A-Z]{2,3}) ([0-9a-z]{3}) -> ([0-9a-z]{3})$").unwrap();
 }
@@ -39,7 +38,9 @@ fn part1(input: &str) -> usize {
                 }
             }
             "XOR" => {
-                if (a_val == Some(&1) && b_val == Some(&0)) || (a_val == Some(&0) && b_val == Some(&1)) {
+                if (a_val == Some(&1) && b_val == Some(&0))
+                    || (a_val == Some(&0) && b_val == Some(&1))
+                {
                     1
                 } else {
                     0
@@ -49,19 +50,71 @@ fn part1(input: &str) -> usize {
         };
         *wires.entry(out.clone()).or_default() = value;
     }
-    for wire in &wires {
-        println!("{:?}", wire);
-    }
-    produce_number(&wires)
+    produce_number(&wires, "z")
 }
 
-fn produce_number(wires: &HashMap<String, u8>) -> usize {
-    let mut output_wires = wires.keys()
-        .filter(|id| id.starts_with("z"))
+fn part2(input: &str) -> String {
+    let (input, gates) = parse(input);
+
+    loop {
+        let mut wires = input.clone();
+        let mut queue = gates.iter().collect::<VecDeque<_>>();
+        while let Some(gate) = queue.pop_front() {
+            let (op, a, b, out) = gate;
+            let (a_val, b_val) = (wires.get(a), wires.get(b));
+            if a_val.is_none() || b_val.is_none() {
+                queue.push_back(gate);
+                continue;
+            }
+            let value = match op.as_str() {
+                "AND" => {
+                    if a_val == Some(&1) && b_val == Some(&1) {
+                        1
+                    } else {
+                        0
+                    }
+                }
+                "OR" => {
+                    if a_val == Some(&1) || b_val == Some(&1) {
+                        1
+                    } else {
+                        0
+                    }
+                }
+                "XOR" => {
+                    if (a_val == Some(&1) && b_val == Some(&0))
+                        || (a_val == Some(&0) && b_val == Some(&1))
+                    {
+                        1
+                    } else {
+                        0
+                    }
+                }
+                _ => unreachable!(),
+            };
+            *wires.entry(out.clone()).or_default() = value;
+        }
+
+        let x = produce_number(&wires, "x");
+        let y = produce_number(&wires, "y");
+        let z = produce_number(&wires, "z");
+        if x + y == z {
+            break;
+        }
+        println!("{} + {} = {}", x, y, z);
+    }
+    String::new()
+}
+
+fn produce_number(wires: &HashMap<String, u8>, prefix: &str) -> usize {
+    let mut output_wires = wires
+        .keys()
+        .filter(|id| id.starts_with(prefix))
         .collect::<Vec<_>>();
     output_wires.sort();
 
-    let binary_output = output_wires.into_iter()
+    let binary_output = output_wires
+        .into_iter()
         .rev()
         .map(|wire| wires.get(wire).unwrap().to_string())
         .collect::<Vec<_>>()
@@ -69,14 +122,11 @@ fn produce_number(wires: &HashMap<String, u8>) -> usize {
     usize::from_str_radix(&binary_output, 2).unwrap()
 }
 
-fn part2(input: &str) -> usize {
-    0
-}
-
 fn parse(input: &str) -> (HashMap<String, u8>, Vec<(String, String, String, String)>) {
     let sections = input.split("\n\n").collect::<Vec<_>>();
 
-    let wires = sections[0].lines()
+    let wires = sections[0]
+        .lines()
         .into_iter()
         .map(|line| {
             let cap = WIRE.captures(line).unwrap();
@@ -84,7 +134,9 @@ fn parse(input: &str) -> (HashMap<String, u8>, Vec<(String, String, String, Stri
         })
         .collect::<HashMap<_, _>>();
 
-    let gates = sections[1].lines().into_iter()
+    let gates = sections[1]
+        .lines()
+        .into_iter()
         .map(|line| {
             let cap = GATE.captures(line).unwrap();
             let a = cap[1].to_string();
@@ -168,7 +220,25 @@ tnw OR pbm -> gnj";
 
     #[test]
     fn test_part2() {
-        let input = "";
-        assert_eq!(part2(input), 0);
+        let input = "x00: 0
+x01: 1
+x02: 0
+x03: 1
+x04: 0
+x05: 1
+y00: 0
+y01: 0
+y02: 1
+y03: 1
+y04: 0
+y05: 1
+
+x00 AND y00 -> z05
+x01 AND y01 -> z02
+x02 AND y02 -> z01
+x03 AND y03 -> z03
+x04 AND y04 -> z04
+x05 AND y05 -> z00";
+        assert_eq!(part2(input), "z00,z01,z02,z05");
     }
 }
